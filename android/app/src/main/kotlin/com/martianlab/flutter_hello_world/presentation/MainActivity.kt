8779869edc8474a9f7d9bd9a76a947854e647760
Oleg Mayorov <o.mayorov@foodplexgroup.ru>
2020-11-17 20:17:57 +0300

    add MethodChannel

--- android/app/src/main/kotlin/com/martianlab/flutter_hello_world/presentation/MainActivity.kt
+++ android/app/src/main/kotlin/com/martianlab/flutter_hello_world/presentation/MainActivity.kt
@@ -3,24 +3,64 @@ package com.martianlab.flutter_hello_world.presentation
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import androidx.annotation.NonNull
 import androidx.fragment.app.viewModels
 import com.martianlab.recipes.domain.RecipesInteractor
 import com.martianlab.recipes.entities.Recipe
 import com.martianlab.recipes.entities.RecipeComplexity
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity(), CoroutineScope by CoroutineScope(Dispatchers.Main) {
+
+    private val CHANNEL = "recipes/platform"
 
     private val interactor: RecipesInteractor by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         println("flutter_srv: "+ "onCreate()")
-        GlobalScope.launch {
-            interactor.firstLaunchCheck()
+        //GlobalScope.launch {
+        //    interactor.firstLaunchCheck()
+        //}
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "firstLaunchCheck" -> {
+                    firstLaunchCheck(call, result)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
         }
     }
+
+    private fun firstLaunchCheck(call: MethodCall, result: MethodChannel.Result) {
+        // get arguments from call
+        //val url = call.argument<String>("url") ?: ""
+        launch {
+            try {
+                interactor?.firstLaunchCheck()
+                result.success(true)
+            } catch (e: Exception) {
+                result.error("N/A",
+                        "Error during firstLaunchCheck().",
+                        e.localizedMessage)
+            }
+        }
+    }
+
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hello_world/constants.dart';
 import 'package:flutter_hello_world/screens/home/home_screen.dart';
+
+import 'interactor/platform/platform.dart';
 
 
 void main() {
@@ -20,7 +23,80 @@ class MyApp extends StatelessWidget {
         textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
         visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      home: HomeScreen(),
+      home: SplashRouterWidget(), //HomeScreen(),
     );
   }
+}
+
+/// Widget for initial routing base on the current session state
+class SplashRouterWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SplashRouterState();
+  }
+}
+
+
+class SplashRouterState extends State {
+  static const platform = const MethodChannel(METHOD_CHANNEL_NAME);
+
+  @override
+  void initState() {
+    _firstLaunchCheck().then((bool hasAccount) {
+      if (hasAccount) {
+        //_signInToLastSession().then(
+        //      (_) => _openProjectsListScreen(),
+        //);
+      } else {
+        //_openAuthScreen();
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+
+  Future<bool> _firstLaunchCheck() async {
+    bool _hasAccount;
+    try {
+      _hasAccount = await platform.invokeMethod('firstLaunchCheck');
+    } on PlatformException catch (_) {
+      // do nothing
+    }
+    return _hasAccount;
+  }
+
+  /// Sign in to last session via platform SDK
+  //Future<void> _signInToLastSession() async {
+  //  try {
+  //    await platform.invokeMethod('signInToLastSession');
+  //  } catch (_) {
+  //    // do nothing
+  //  }
+  //}
+
+  /// Navigate to the [ProjectsListScreen].
+  //void _openAuthScreen() {
+  //  Navigator.pushReplacement(
+  //    context,
+  //    MaterialPageRoute(
+  //      builder: (context) => AuthScreen(),
+  //    ),
+  //  );
+  //}
+
+  /// Navigate to the [ProjectsListScreen].
+  //void _openProjectsListScreen() {
+  //  Navigator.pushReplacement(
+  //    context,
+  //    MaterialPageRoute(
+  //      builder: (context) => ProjectsListScreen(),
+  //    ),
+  //  );
+  //}
 }
